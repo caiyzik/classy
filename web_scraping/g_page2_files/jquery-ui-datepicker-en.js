2d9071e3b2df3916ac3c671bd57f19c833a3de13
trmontgomery <tiffany.re.montgomery@gmail.com>
2019-10-11 00:46:16 +0200

    tidying

--- web_scraping/g_page2_files/jquery-ui-datepicker-en.js
+++
@@ -1,6 +0,0 @@
-/* Inicializacion en español para la extensión 'UI date picker' para jQuery. */
-jQuery(function($){
-	$.datepicker.regional['en'] = {
-		dateFormat: 'dd/mm/yy'};
-	$.datepicker.setDefaults($.datepicker.regional['en']);
-});
